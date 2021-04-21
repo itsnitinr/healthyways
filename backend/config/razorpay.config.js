1643ae2b615189440a91641b351ce82487dd96e2
Nitin Ranganath <nitinranganath@gmail.com>
2021-04-21 22:43:36 +0530

    Razorpay configuration

---
+++ backend/config/razorpay.config.js
@@ -0,0 +1,8 @@
+const Razorpay = require('razorpay');
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+module.exports = razorpay;
